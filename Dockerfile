fe8ff5aa035cf025f01bbce2c28f70552fca8f6e
Johan Tjelldén <johan@hedvig.com>
2017-09-28 15:38:46 +0200

    Fix for travis deployment

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:8
 
 
-ADD target/must-rename-0.0.1-SNAPSHOT.jar /
+ADD target/bot-service-0.0.1-SNAPSHOT.jar /
 
-ENTRYPOINT java -jar must-rename-0.0.1-SNAPSHOT.jar
+ENTRYPOINT java -jar bot-service-0.0.1-SNAPSHOT.jar
--- bin/kubernetes_deploy.sh
+++ bin/kubernetes_deploy.sh
@@ -2,4 +2,4 @@
 
 aws s3 cp s3://dev-com-hedvig-cluster-ett-data/kube ~/.kube --recursive
 
-kubectl set image deployment/must-rename-deployment must-rename-deployment=$REMOTE_IMAGE_URL:$TRAVIS_BUILD_NUMBER
+kubectl set image deployment/bot-service bot-service=$REMOTE_IMAGE_URL:$TRAVIS_BUILD_NUMBER
