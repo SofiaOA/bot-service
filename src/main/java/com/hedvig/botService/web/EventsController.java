0bd1836e79b6eb64a1254c0f1f19e148a4dfed37
Johan Tjelldén <johan@hedvig.com>
2017-10-17 13:57:14 +0200

    Added MemberEvents controller

---
+++ src/main/java/com/hedvig/botService/web/EventsController.java
@@ -0,0 +1,29 @@
+package com.hedvig.botService.web;
+
+import com.hedvig.botService.session.SessionManager;
+import com.hedvig.botService.web.dto.MemberAuthedEvent;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventsController {
+
+    private final SessionManager sessionManager;
+
+    public EventsController(SessionManager sessionManager) {
+
+        this.sessionManager = sessionManager;
+    }
+
+
+    @PostMapping("/event/memberservice")
+    public ResponseEntity<String> memberservice(@RequestBody MemberAuthedEvent event) {
+
+        sessionManager.receiveEvent(event);
+
+        return ResponseEntity.ok("");
+    }
+
+}
