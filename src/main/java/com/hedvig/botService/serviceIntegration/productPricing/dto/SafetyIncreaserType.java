188d716a2bf28cc3f71bd3bac78817f4c9e67dec
Johan Tjelldén <johan@hedvig.com>
2017-11-03 11:16:20 +0100

    Support new safetyincreasers

---
+++ src/main/java/com/hedvig/botService/serviceIntegration/productPricing/dto/SafetyIncreaserType.java
@@ -0,0 +1,9 @@
+package com.hedvig.botService.serviceIntegration.productPricing.dto;
+
+public enum SafetyIncreaserType {
+    SMOKE_ALARM,
+    FIRE_EXTINGUISHER,
+    SAFETY_DOOR,
+    GATE,
+    BURGLAR_ALARM
+}
