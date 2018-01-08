06103c4a18191fc53895a0f3a914db568174d95a
Johan Tjelldén <johan@hedvig.com>
2018-01-08 13:51:15 +0100

    Add swagger support

--- src/main/java/com/hedvig/botService/BotServiceApplication.java
+++ src/main/java/com/hedvig/botService/BotServiceApplication.java
@@ -17,11 +17,17 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableRetry
 @EnableTransactionManagement
+@EnableSwagger2
 public class BotServiceApplication {
 
 	public static void main(String[] args) {
@@ -47,4 +53,13 @@ public class BotServiceApplication {
     public MemberService createMemberService() {
         return new MemberServiceFake();
    }*/
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
