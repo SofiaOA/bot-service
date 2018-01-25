6110770a360387d42b366aa9f2500044096d476f
John <john@hedvig.com>
2018-01-25 15:25:40 +0100

    Underwriting limits and copy changes

--- src/main/java/com/hedvig/botService/dataTypes/LivingSpaceSquareMeters.java
+++ src/main/java/com/hedvig/botService/dataTypes/LivingSpaceSquareMeters.java
@@ -9,8 +9,8 @@ public class LivingSpaceSquareMeters extends HedvigDataType {
 	public boolean validate(String input) {
 		try{
 			Integer m = Integer.parseInt(input);
-			if(m<5){ this.errorMessage = input + "kvm låter väldigt litet. Prova att ange igen tack"; return false;}
-			if(m>400){ this.errorMessage = input + "kvm?! Kan man bo så stort? Hmm... Prova att ange igen tack"; return false; }
+			if(m<5){ this.errorMessage = input + "kvm låter väldigt litet. Prova igen tack!"; return false;}
+			if(m>400){ this.errorMessage = input + "kvm?! Kan man bo så stort? Hmm... Prova igen tack!"; return false; }
 		}catch(NumberFormatException e){
 			this.errorMessage = input + " verkar vara ett konstigt antal kvadratmeter. Prova igen tack";
 			return false;
