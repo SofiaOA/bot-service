56663c97e5161b713cabbffda78b143090c798fe
Johan Tjelldén <johan@hedvig.com>
2018-02-13 10:42:25 +0100

    Only load swagger during development

--- src/main/java/com/hedvig/botService/BotServiceApplication.java
+++ src/main/java/com/hedvig/botService/BotServiceApplication.java
@@ -7,10 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -36,12 +32,4 @@ public class BotServiceApplication {
         return new MemberServiceFake();
    }*/
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
 }
