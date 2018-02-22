b81f74af86a33dcc46bff9f8a47ee0f99b9793d7
Johan Tjelldén <johan@hedvig.com>
2018-02-22 16:38:22 +0100

    Log with parameters

--- src/main/java/com/hedvig/botService/web/TriggerController.java
+++ src/main/java/com/hedvig/botService/web/TriggerController.java
@@ -75,14 +75,14 @@ public class TriggerController {
         try {
             final String redirectionUrl = triggerService.clientNotificationReceived(triggerId, status);
 
-            log.info("Redirecting to: ", redirectionUrl);
+            log.info("Redirecting to: {}", redirectionUrl);
             return ResponseEntity.status(304).location(URI.create(redirectionUrl)).build();//.location()
         }
         catch(Exception ex) {
             log.error("Exception caught in TriggerController.getNotificaiton, redirecting to " + errorPageUrl, ex);
         }
 
-        log.info("Redirecting to: ", this.errorPageUrl);
+        log.info("Redirecting to: {}", this.errorPageUrl);
         return ResponseEntity.status(304).location(this.errorPageUrl).build();
     }
 
