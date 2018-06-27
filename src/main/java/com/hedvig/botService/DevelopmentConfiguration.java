abb47b63fbc967e07932e2b9d65c561ffe9ccc35
Johan Tjelldén <johan@hedvig.com>
2018-06-27 18:52:55 +0200

    Enable swagger in staging

--- src/main/java/com/hedvig/botService/DevelopmentConfiguration.java
+++ src/main/java/com/hedvig/botService/DevelopmentConfiguration.java
@@ -10,7 +10,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@Profile("development")
+@Profile({"development", "staging"})
 @EnableSwagger2
 public class DevelopmentConfiguration {
 
