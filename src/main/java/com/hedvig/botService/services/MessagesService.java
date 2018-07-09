d018784cba2364ec1aa12642635fd5ce63ba6672
meletios <meletios@hedvig.com>
2018-07-09 12:02:10 +0200

    enable claim fab when the customer have no active claim

--- src/main/java/com/hedvig/botService/services/MessagesService.java
+++ src/main/java/com/hedvig/botService/services/MessagesService.java
@@ -39,11 +39,11 @@ public class MessagesService {
 
         val messages = uc.getMessages(intent, conversationFactory);
 
-        val hasClaim = this.claimsService.getActiveClaims(hid) > 0;
+        Boolean hasClaim = this.claimsService.getActiveClaims(hid) > 0;
 
         final String triggerUrl = "/v2/app/fabTrigger/%s";
         val options = Lists.newArrayList(
-                new MessagesDTO.FABOption("Anmäl en skada", String.format(triggerUrl, FABAction.REPORT_CLAIM.name()), true),
+                new MessagesDTO.FABOption("Anmäl en skada", String.format(triggerUrl, FABAction.REPORT_CLAIM.name()), !hasClaim),
                 new MessagesDTO.FABOption("Prata med Hedvig", String.format(triggerUrl, FABAction.CHAT.name()), true),
                 new MessagesDTO.FABOption("Det är kris! Ring mig", String.format(triggerUrl, FABAction.CALL_ME.name()), true)
 
