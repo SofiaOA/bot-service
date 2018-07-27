14f6855699453e929182f64ec7fc7c9f0a454abc
Oscar Nylander <oscar@hedvig.com>
2018-07-27 16:16:07 +0200

    Hotfix the callme-conversation being protected

--- src/main/java/com/hedvig/botService/chat/CallMeConversation.java
+++ src/main/java/com/hedvig/botService/chat/CallMeConversation.java
@@ -6,7 +6,6 @@ import com.hedvig.botService.enteties.message.*;
 import com.hedvig.botService.services.events.RequestPhoneCallEvent;
 import lombok.val;
 import org.springframework.context.ApplicationEventPublisher;
-
 import java.util.List;
 import java.util.Objects;
 
@@ -20,27 +19,20 @@ public class CallMeConversation extends Conversation {
     private final StatusBuilder statusBuilder;
     private final ApplicationEventPublisher eventPublisher;
 
-    public CallMeConversation(StatusBuilder statusBuilder, ApplicationEventPublisher eventPublisher) {
+    public CallMeConversation(StatusBuilder statusBuilder,
+            ApplicationEventPublisher eventPublisher) {
         this.statusBuilder = statusBuilder;
         this.eventPublisher = eventPublisher;
-        createMessage(
-                CALLME_CHAT_START,
-                new MessageHeader(Conversation.HEDVIG_USER_ID, -1, false),
+        createMessage(CALLME_CHAT_START, new MessageHeader(Conversation.HEDVIG_USER_ID, -1, false),
                 new MessageBodySingleSelect("Hej {NAME}, ska jag ringa dig på {PHONE_NUMBER}?",
-                        Lists.newArrayList(
-                                new SelectOption("Ja", CALLME_PHONE_OK),
+                        Lists.newArrayList(new SelectOption("Ja", CALLME_PHONE_OK),
                                 new SelectOption("Nej", CALLME_PHONE_CHANGE))));
 
-        createMessage(
-                CALLME_PHONE_OK,
-                new MessageHeader(Conversation.HEDVIG_USER_ID, -1, true),
-                new MessageBodySingleSelect("Ok då ser jag till att någon ringer dig?",
-                        Lists.newArrayList(
-                                SelectLink.toDashboard("Ok", "callme.phone.dashboard"))));
+        createMessage(CALLME_PHONE_OK, new MessageHeader(Conversation.HEDVIG_USER_ID, -1, true),
+                new MessageBodySingleSelect("Ok då ser jag till att någon ringer dig?", Lists
+                        .newArrayList(SelectLink.toDashboard("Ok", "callme.phone.dashboard"))));
 
-        createMessage(
-                CALLME_PHONE_CHANGE,
-                new MessageHeader(Conversation.HEDVIG_USER_ID, -1, true),
+        createMessage(CALLME_PHONE_CHANGE, new MessageHeader(Conversation.HEDVIG_USER_ID, -1, true),
                 new MessageBodyText("Vilket telefonnummer kan jag nå dig på?"));
     }
 
@@ -51,17 +43,17 @@ public class CallMeConversation extends Conversation {
 
     @Override
     public boolean canAcceptAnswerToQuestion(UserContext uc) {
-        return false;
+        return true;
     }
 
     @Override
     public void receiveMessage(UserContext userContext, Message m) {
         String nxtMsg = "";
 
-        switch (m.getBaseMessageId()){
+        switch (m.getBaseMessageId()) {
             case CALLME_PHONE_CHANGE: {
                 String trimmedText = m.body.text.trim();
-                //userContext.putUserData("{PHONE_NUMBER}", trimmedText);
+                // userContext.putUserData("{PHONE_NUMBER}", trimmedText);
                 m.body.text = "Ni kan nå mig på telefonnummer " + trimmedText;
                 addToChat(m, userContext);
 
@@ -72,20 +64,21 @@ public class CallMeConversation extends Conversation {
             case CALLME_CHAT_START: {
                 val messageBody = (MessageBodySingleSelect) m.body;
                 val selectedItem = messageBody.getSelectedItem();
-                if(Objects.equals(selectedItem.value, CALLME_PHONE_OK)) {
+                if (Objects.equals(selectedItem.value, CALLME_PHONE_OK)) {
                     endConversation(userContext, m);
                 }
             }
         }
 
         /*
-         * In a Single select, there is only one trigger event. Set default here to be a link to a new message
+         * In a Single select, there is only one trigger event. Set default here to be a link to a
+         * new message
          */
         if (nxtMsg.equals("") && m.body.getClass().equals(MessageBodySingleSelect.class)) {
 
             MessageBodySingleSelect body1 = (MessageBodySingleSelect) m.body;
             for (SelectItem o : body1.choices) {
-                if(o.selected) {
+                if (o.selected) {
                     m.body.text = o.text;
                     addToChat(m, userContext);
                     nxtMsg = o.value;
@@ -97,7 +90,9 @@ public class CallMeConversation extends Conversation {
     }
 
     private void endConversation(UserContext userContext, Message m) {
-        eventPublisher.publishEvent(new RequestPhoneCallEvent(userContext.getMemberId(), m.body.text, userContext.getOnBoardingData().getFirstName(), userContext.getOnBoardingData().getFamilyName()));
+        eventPublisher.publishEvent(new RequestPhoneCallEvent(userContext.getMemberId(),
+                m.body.text, userContext.getOnBoardingData().getFirstName(),
+                userContext.getOnBoardingData().getFamilyName()));
         userContext.completeConversation(this);
     }
 
