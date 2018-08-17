fd42677508d68f3bad6461a26dd9189ad511b43f
Oscar Nylander <oscar@hedvig.com>
2018-08-17 09:56:44 +0200

    Move get push token for member to internal controller

--- src/main/java/com/hedvig/botService/web/v2/PushTokenController.java
+++ src/main/java/com/hedvig/botService/web/v2/PushTokenController.java
@@ -2,15 +2,12 @@ package com.hedvig.botService.web.v2;
 
 import com.hedvig.botService.services.SessionManager;
 import com.hedvig.botService.web.v2.dto.RegisterPushTokenRequest;
-import com.hedvig.botService.web.v2.dto.PushTokenResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import lombok.val;
 
 @RestController
 @RequestMapping("/v2/push-token")
@@ -26,15 +23,5 @@ public class PushTokenController {
       @RequestHeader(value = "hedvig.token") String hid) {
     sessionManager.saveFirebasePushToken(hid, dto.getFcmRegistrationToken());
     return ResponseEntity.noContent().build();
-  }
-
-  @GetMapping
-  public ResponseEntity<PushTokenResponse> pushToken(
-      @RequestHeader(value = "hedvig.token") String hid) {
-    val token = sessionManager.getFirebasePushToken(hid);
-    if (token == null) {
-      return ResponseEntity.notFound().build();
-    }
-    return ResponseEntity.ok(new PushTokenResponse(token));
   }
 }
